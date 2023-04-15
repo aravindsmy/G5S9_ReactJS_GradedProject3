aab584f58b4ab8e94680ef59a1fca9ddc086229d
aravindsmy <107797044+aravindsmy@users.noreply.github.com>
2023-04-15 23:20:10 +0530

    Submitting React assignment solution

---
+++ moviesapp/src/styles/Theme.ts
@@ -0,0 +1,52 @@
+const theme = {
+    fonts: {
+        styles: {
+            card: "'Ubuntu', sans-serif",
+            paragraph: "'Heebo', sans-serif",
+            heading: "'Poppins', sans-serif"
+        },
+        sizes: {
+            small: "0.5rem",
+            base: "0.875rem",
+            medium: "1rem",
+            large: "1.5rem",
+            xlarge: "2.4rem",
+            xxlarge: "4rem"
+        }
+    },
+    colors: {
+        forecolor: "#e2e2e2",
+        background_main: "rgb(2,0,36)",
+        background_grad: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(10,10,73,0.9139005944174545) 100%)",
+        accentcolor_main: "rgb(30,85,208)",
+        accentcolor_grad: "linear-gradient(90deg, rgba(30,85,208,1) 0%, rgba(14,40,89,0.9587185215883228) 100%)",
+        successcolor: '#0045de',
+        errorcolor: '#4b5f8b',
+        buttonbackcolor: "linear-gradient(90deg, rgba(0,4,29,0.28925073447347693) 0%, rgba(8,7,82,0.23322832550989148) 100%)",
+        buttonforecolor: "#00041D"
+    },
+    padding: {
+        small: "0.5rem",
+        base: "0.7rem",
+        medium: "1.1rem",
+        large: "2rem",
+        xlarge: "3.5rem"
+    },
+
+    margin: {
+        tiny: "2px",
+        small: "0.5rem",
+        base: "0.7rem",
+        medium: "1.1rem",
+        large: "2rem",
+        xlarge: "3.5rem"
+    },
+
+    screen: {
+        mobile: "638px",
+        tablet: "998px",
+        desktop: "1200px"
+    }
+}
+
+export default theme;
